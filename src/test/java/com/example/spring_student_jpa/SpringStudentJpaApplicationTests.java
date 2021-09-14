30b912a559ac601a9ff206fb06383a0103761c4f
unknown <nghiazexal@gmail.com>
2021-09-14 13:05:18 +0700

    @test save of repo

--- src/test/java/com/example/spring_student_jpa/SpringStudentJpaApplicationTests.java
+++ src/test/java/com/example/spring_student_jpa/SpringStudentJpaApplicationTests.java
@@ -1,13 +1,46 @@
 package com.example.spring_student_jpa;
 
+import com.example.spring_student_jpa.entity.Student;
+import com.example.spring_student_jpa.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class SpringStudentJpaApplicationTests {
 
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+
+    // test các repo
+
     @Test
-    void contextLoads() {
+    private void insertStudent(){
+        Student student=Student.builder().id(12L).name("tran nghia").email("nghia2knt@gmail.com").build();
+        studentRepository.save(student);
+    }
+
+    @Test
+    private void printAllStudent(){
+        List<Student> studentList = (List<Student>) studentRepository.findAll();
+        System.out.println("List Student: " +studentList);
+    }
+
+    @Test
+    private void getStudentById(){
+        Optional<Student> student = studentRepository.findById(12L);
+        System.out.println("Student: " + student);
+    }
+
+    @Test
+    private void deleteStudentById(){
+        studentRepository.deleteById(12L);
+        System.out.println("deleted!");
     }
 
 }
