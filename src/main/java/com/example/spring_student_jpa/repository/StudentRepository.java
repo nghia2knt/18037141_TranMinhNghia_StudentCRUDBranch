db2a3b4fdcacbd102aa21be7b2430620e25cee32
unknown <nghiazexal@gmail.com>
2021-09-14 14:53:33 +0700

    Finish crud in native

--- src/main/java/com/example/spring_student_jpa/repository/StudentRepository.java
+++ src/main/java/com/example/spring_student_jpa/repository/StudentRepository.java
@@ -1,8 +1,10 @@
 package com.example.spring_student_jpa.repository;
 
 import com.example.spring_student_jpa.entity.Student;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,17 +15,19 @@ import java.util.List;
 @Repository
 public interface StudentRepository  extends CrudRepository<Student,Long> {
 
-    @PersistenceContext
-    public EntityManager entityManager = null;
 
-    @Transactional
-    public default void insertWithQuery(Student student) {
-        entityManager.createNativeQuery("INSERT INTO tbl_student (id, email, name) VALUES (?,?,?)")
-                .setParameter(1, student.getId())
-                .setParameter(2, student.getEmail())
-                .setParameter(3, student.getName())
-                .executeUpdate();
-    }
+    @Modifying
+    @Query(
+            value = "INSERT INTO tbl_student VALUES (:id,:email,:name)",
+            nativeQuery = true)
+    void insertInNative(@Param("id") Long id, @Param("email") String email, @Param("name") String name);
+
+    @Modifying
+    @Query(value = "update tbl_student u set u.email = ?, u.name = ?  where u.id = ?",
+            nativeQuery = true)
+    int updateInNative(String email, String name, Long id);
+
+
     @Query(
             value = "select * from tbl_student",
             nativeQuery = true
--- src/test/java/com/example/spring_student_jpa/SpringStudentJpaApplicationTests.java
+++ src/test/java/com/example/spring_student_jpa/SpringStudentJpaApplicationTests.java
@@ -9,15 +9,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringStudentJpaApplicationTests {
 
-   @Autowired
+    @Autowired
     private StudentRepository studentRepository;
 
-   @Test
+    @Test
     private void insertTest(){
        Student student=Student.builder().name("tran nghia").email("nghia2knt@gmail.com").build();
-       studentRepository.insertWithQuery(student);
+       studentRepository.insertInNative(student.getId(), student.getEmail(),student.getName());
    }
-
+    @Test
+    private void updateTest(){
+        Student student=Student.builder().name("tran Nam").email("name2knt@gmail.com").build();
+        Long id = student.getId();
+        studentRepository.insertInNative(student.getId(), student.getEmail(),student.getName());
+        studentRepository.updateInNative("edit@gmail.com","ten da edit",id);
+    }
    @Test
     private void getTest(){
        studentRepository.findAllInNative();
