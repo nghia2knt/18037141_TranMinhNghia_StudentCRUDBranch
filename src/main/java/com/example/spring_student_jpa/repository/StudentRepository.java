1cbcc9642604df745ffc784aa7d6cecbe05e8da2
unknown <nghiazexal@gmail.com>
2021-09-14 14:10:47 +0700

    commit for crud native

--- src/main/java/com/example/spring_student_jpa/repository/StudentRepository.java
+++ src/main/java/com/example/spring_student_jpa/repository/StudentRepository.java
@@ -1,10 +1,38 @@
 package com.example.spring_student_jpa.repository;
 
 import com.example.spring_student_jpa.entity.Student;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public interface StudentRepository  extends CrudRepository<Student,Long> {
 
+    @PersistenceContext
+    public EntityManager entityManager = null;
+
+    @Transactional
+    public default void insertWithQuery(Student student) {
+        entityManager.createNativeQuery("INSERT INTO tbl_student (id, email, name) VALUES (?,?,?)")
+                .setParameter(1, student.getId())
+                .setParameter(2, student.getEmail())
+                .setParameter(3, student.getName())
+                .executeUpdate();
+    }
+    @Query(
+            value = "select * from tbl_student",
+            nativeQuery = true
+    )
+    List<Student> findAllInNative();
+
+    @Query(
+            value = "delete * from tbl_student",
+            nativeQuery = true
+    )
+    void deleteAllInNative();
 }
--- src/test/java/com/example/spring_student_jpa/SpringStudentJpaApplicationTests.java
+++ src/test/java/com/example/spring_student_jpa/SpringStudentJpaApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.spring_student_jpa;
 
+import com.example.spring_student_jpa.entity.Student;
+import com.example.spring_student_jpa.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringStudentJpaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+   @Autowired
+    private StudentRepository studentRepository;
 
+   @Test
+    private void insertTest(){
+       Student student=Student.builder().name("tran nghia").email("nghia2knt@gmail.com").build();
+       studentRepository.insertWithQuery(student);
+   }
+
+   @Test
+    private void getTest(){
+       studentRepository.findAllInNative();
+   }
+
+   @Test
+    private void deleteTest(){
+       studentRepository.deleteAllInNative();
+   }
 }
