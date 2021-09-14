e4f36c520cfb6c8933128c39e55544c96c34f5da
unknown <nghiazexal@gmail.com>
2021-09-14 15:14:43 +0700

    finish crud in jpql

--- src/main/java/com/example/spring_student_jpa/repository/StudentRepository.java
+++ src/main/java/com/example/spring_student_jpa/repository/StudentRepository.java
@@ -1,10 +1,36 @@
 package com.example.spring_student_jpa.repository;
 
 import com.example.spring_student_jpa.entity.Student;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface StudentRepository  extends CrudRepository<Student,Long> {
 
+    // insert sử dụng cả native và Modifying
+    @Modifying
+    @Query(
+            value = "INSERT INTO tbl_student VALUES (:id,:email,:name)",
+            nativeQuery = true)
+    void insertInJPQL(@Param("id") Long id, @Param("email") String email, @Param("name") String name);
+
+
+    @Modifying
+    @Query("update Student s set s.email = :email, s.name = :name where s.id = :id")
+    void updateInJPQL(@Param("email") String email,
+                                   @Param("name") String name,
+                                   @Param("id") Long id);
+
+
+    @Query("SELECT s FROM Student s")
+    List<Student> findInJPQL();
+
+    @Query("Delete from Student")
+    void deleteInJPQL();
 }
--- src/test/java/com/example/spring_student_jpa/SpringStudentJpaApplicationTests.java
+++ src/test/java/com/example/spring_student_jpa/SpringStudentJpaApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.spring_student_jpa;
 
+import com.example.spring_student_jpa.entity.Student;
+import com.example.spring_student_jpa.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringStudentJpaApplicationTests {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
     @Test
-    void contextLoads() {
+    private void insertTest(){
+        Student student = Student.builder().email("nghia2knt@gmail.com").name("nghia 123").build();
+        studentRepository.insertInJPQL(student.getId(),student.getEmail(),student.getName());
+    }
+    @Test
+    private void updateTest(){
+        Student student = Student.builder().email("nghiaTest@gmail.com").name("nghia 456").build();
+        Long id = student.getId();
+        studentRepository.insertInJPQL(student.getId(),student.getEmail(),student.getName());
+        studentRepository.updateInJPQL("nghiaedit@gmail.com","nghia edit",id);
+
+    }
+
+    @Test
+    private void getTest(){
+        studentRepository.findInJPQL();
+    }
+
+    @Test
+    private void deleteTest(){
+        studentRepository.deleteInJPQL();
     }
 
 }
